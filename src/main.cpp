add8d61a62acd64f95f9fbfd1a86be50fefaf817
Dmitry Morozov <9984398@gmail.com>
2022-09-23 21:33:36 +0300

    HotFix : Class dotenv renamed to cdotenv

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,32 @@
 ///3-d Party libs
 
-#include "dotenv.h"
 #include <tgbot/tgbot.h>
 
 
 #include <stdio.h>
 
 
-int main() {
-    TgBot::Bot bot("5664821903:AAGuQg1A08I2YmO4hVsQGwsLp-aw5GTDHrU");
-    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "Hi!");
+int main () {
+    TgBot::Bot bot ("5664821903:AAGuQg1A08I2YmO4hVsQGwsLp-aw5GTDHrU");
+    bot.getEvents ().onCommand ("start", [ &bot ] (TgBot::Message::Ptr message) {
+        bot.getApi ().sendMessage (message->chat->id, "Hi!");
     });
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
-        printf("User wrote %s\n", message->text.c_str());
-        if (StringTools::startsWith(message->text, "/start")) {
+    bot.getEvents ().onAnyMessage ([ &bot ] (TgBot::Message::Ptr message) {
+        printf ("User wrote %s\n", message->text.c_str ());
+        if (StringTools::startsWith (message->text, "/start")) {
             return;
         }
-        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+        bot.getApi ().sendMessage (message->chat->id, "Your message is: " + message->text);
     });
     try {
-        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
-        TgBot::TgLongPoll longPoll(bot);
+        printf ("Bot username: %s\n", bot.getApi ().getMe ()->username.c_str ());
+        TgBot::TgLongPoll longPoll (bot);
         while (true) {
-            printf("Long poll started\n");
-            longPoll.start();
+            printf ("Long poll started\n");
+            longPoll.start ();
         }
-    } catch (TgBot::TgException& e) {
-        printf("error: %s\n", e.what());
+    } catch (TgBot::TgException &e) {
+        printf ("error: %s\n", e.what ());
     }
     return 0;
 }
