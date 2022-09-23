4432f284a6773557d1c23f99382d8ffd6bca4cf2
Dmitry Morozov <9984398@gmail.com>
2022-09-24 00:39:18 +0300

    Structure refactored

---
+++ include/load_api_keys.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Dmitry Morozov on 23/9/22.
+//
+
+#ifndef ECHOBOT_DEV_LOAD_API_KEYS_HPP
+#define ECHOBOT_DEV_LOAD_API_KEYS_HPP
+
+#endif //ECHOBOT_DEV_LOAD_API_KEYS_HPP
---
+++ src/load_api_keys.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dmitry Morozov on 23/9/22.
+//
+
+#include "load_api_keys.hpp"
