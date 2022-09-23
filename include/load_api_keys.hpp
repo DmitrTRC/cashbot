b7f0bae2eac461195c9cdc80855cf46f069873a7
Dmitry Morozov <9984398@gmail.com>
2022-09-24 00:57:27 +0300

    Load_api_keys : Done Tests : None

--- include/load_api_keys.hpp
+++ include/load_api_keys.hpp
@@ -5,4 +5,11 @@
 #ifndef ECHOBOT_DEV_LOAD_API_KEYS_HPP
 #define ECHOBOT_DEV_LOAD_API_KEYS_HPP
 
+#include <string>
+#include <map>
+
+
+std::map<std::string, std::string> load_environment (const std::string &);
+
 #endif //ECHOBOT_DEV_LOAD_API_KEYS_HPP
+
--- src/load_api_keys.cpp
+++ src/load_api_keys.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "load_api_keys.hpp"
+
+#include <fstream>
+#include <map>
+#include <sstream>
+
+
+/// Read API keys from env file
+std::map<std::string, std::string> load_environment (const std::string &file_name = "bot.env") {
+    std::map<std::string, std::string> env;
+    std::ifstream env_file (file_name);
+
+    if (env_file.is_open ()) {
+        std::string line;
+
+        while (std::getline (env_file, line)) {
+            if (line.empty ()) {
+                continue;
+            }
+            std::istringstream line_stream (line);
+            std::string key;
+            if (std::getline (line_stream, key, '=')) {
+                std::string value;
+                if (std::getline (line_stream, value)) {
+                    env[key] = value;
+                }
+            }
+        }
+    }
+
+    return env;
+}
