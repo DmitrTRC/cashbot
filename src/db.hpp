bc059d61f54f9d5e11a9f917147a8b0f71d60338
Dmitry Morozov <9984398@gmail.com>
2022-10-02 01:27:38 +0300

    ORM_MIDDLEWARE : Initial commit

--- src/db.hpp
+++ src/db.hpp
@@ -5,4 +5,15 @@
 #ifndef CASHBOT_FEATURE_DB_DB_HPP
 #define CASHBOT_FEATURE_DB_DB_HPP
 
+
+#include "db_structure.hpp"
+
+#include <sqlite_orm/sqlite_orm.h>
+#include <string>
+#include <iostream>
+
+
+using namespace sqlite_orm;
+
+
 #endif //CASHBOT_FEATURE_DB_DB_HPP
