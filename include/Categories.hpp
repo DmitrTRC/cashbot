3e6b3f54d7859b31cc707259a610612086584fdb
Dmitry Morozov <9984398@gmail.com>
2022-10-02 23:14:17 +0300

    Class categories : Initial commit

---
+++ include/Categories.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Dmitry Morozov on 2/10/22.
+//
+
+#ifndef CASHBOT_FEATURE_DB_CATEGORIES_HPP
+#define CASHBOT_FEATURE_DB_CATEGORIES_HPP
+
+#include "db_structure.hpp"
+
+
+class Categories {
+
+};
+
+
+#endif //CASHBOT_FEATURE_DB_CATEGORIES_HPP
