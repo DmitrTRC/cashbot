fdfa287573e4033c5bc4635785874d5a67e40f2c
Dmitry Morozov <9984398@gmail.com>
2022-10-03 00:00:13 +0300

    Categories.cpp : _fill_aliases : WontFix

--- include/Categories.hpp
+++ include/Categories.hpp
@@ -7,8 +7,26 @@
 
 #include "db_structure.hpp"
 
+#include <vector>
+#include <map>
+#include <string>
+
 
 class Categories {
+public:
+    Categories ();
+
+    ~Categories ();
+
+    DB::TCategories getAllCategories ();
+
+    DB::Category getCategory (const std::string &);
+
+    DB::TCategories _fill_aliases (std::vector<std::map<std::string, std::string>> &);
+
+private:
+    DB::TCategories _categories;
+
 
 };
 
--- src/Categories.cpp
+++ src/Categories.cpp
@@ -3,3 +3,58 @@
 //
 
 #include "Categories.hpp"
+
+
+DB::TCategories Categories::getAllCategories () {
+    return _categories;
+}
+
+DB::Category Categories::getCategory (const std::string &categoryName) {
+    DB::Category resultCategory;
+    DB::Category otherCategory;
+
+
+    for (auto &category: _categories) {
+
+        if (category.codename == categoryName) {
+            otherCategory = category;
+        }
+
+        for (auto &alias: category.aliases) {
+            if (std::find (category.aliases.begin (), category.aliases.end (), categoryName) !=
+                category.aliases.end ()) {
+                resultCategory = category;
+            }
+
+        }
+
+    }
+    if (resultCategory.codename.empty ()) {
+        resultCategory = otherCategory;
+    }
+    return resultCategory;
+}
+
+DB::TCategories Categories::_fill_aliases (std::vector<std::map<std::string, std::string>> &) {
+    DB::TCategories categories_result;
+
+    size_t index = 0;
+    //FIXME: Wrong logic
+//    for (auto &category: _categories) {
+//        std::vector<std::string> aliases;
+//        for (auto &alias: _categories[index].aliases) {
+//            aliases.push_back (alias);
+//        }
+//        categories_result.push_back (DB::Category {
+//                .codename = category.codename,
+//                .name = category.name,
+//                .is_base_expense = category.is_base_expense,
+//                .aliases = aliases
+//        });
+//        index++;
+//    }
+
+    return categories_result;
+}
+
+
