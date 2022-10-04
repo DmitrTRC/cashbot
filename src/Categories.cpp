119afab6e79b50cd5f5d808df7a20902a1a5d90d
Dmitry Morozov <9984398@gmail.com>
2022-10-04 18:58:25 +0300

    Merged with feature/category

--- src/Categories.cpp
+++ src/Categories.cpp
@@ -8,11 +8,13 @@
 #include <boost/algorithm/string.hpp>
 
 
-DB::TCategories Categories::getAllCategories () {
+DB::TCategories Categories::getAllCategories() {
+
     return _categories;
 }
 
-DB::Category Categories::getCategory (const std::string &categoryName) {
+DB::Category Categories::getCategory(const std::string &categoryName) {
+
     DB::Category resultCategory;
     DB::Category otherCategory;
 
@@ -24,29 +26,30 @@ DB::Category Categories::getCategory (const std::string &categoryName) {
         }
 
         for (auto &alias: category.aliases) {
-            if (alias.find (categoryName) != std::string::npos) {
+            if (alias.find(categoryName) != std::string::npos) {
                 resultCategory = category;
             }
 
         }
 
     }
-    if (resultCategory.codename.empty ()) {
+    if (resultCategory.codename.empty()) {
         resultCategory = otherCategory;
     }
     return resultCategory;
 }
 
 
-DB::TCategories Categories::_fill_aliases (std::vector<std::map<std::string, std::string>> &categories) {
+DB::TCategories Categories::_fill_aliases(std::vector<std::map<std::string, std::string>> &categories) {
+
     DB::TCategories resultCategories;
 
     for (auto &category: categories) {
         std::vector<std::string> aliases;
-        boost::split (aliases, category["aliases"], boost::is_any_of (","));
-        aliases.push_back (category["codename"]);
-        aliases.push_back (category["name"]);
-        resultCategories.push_back (DB::Category{
+        boost::split(aliases, category["aliases"], boost::is_any_of(","));
+        aliases.push_back(category["codename"]);
+        aliases.push_back(category["name"]);
+        resultCategories.push_back(DB::Category{
                 .codename = category["codename"],
                 .name = category["name"],
                 .is_base_expense = category["is_base_expense"] == "1",
@@ -58,19 +61,21 @@ DB::TCategories Categories::_fill_aliases (std::vector<std::map<std::string, std
 
 }
 
-DB::TCategories Categories::_getCategoriesFromDB () {
+DB::TCategories Categories::_getCategoriesFromDB() {
+
     std::vector<std::string> columns = {"codename", "name", "is_base_expense", "aliases"};
     std::string query_name = "category";
 
-    std::vector<std::map<std::string, std::string >> categories = _db_handler->fetchAll (query_name, columns);
-    auto result = _fill_aliases (categories);
+    std::vector<std::map<std::string, std::string >> categories = _db_handler->fetchAll(query_name, columns);
+    auto result = _fill_aliases(categories);
 
-
+    return {}; //TODO: return result! (now it's empty)
 }
 
-Categories::Categories (botDB *db) {
+Categories::Categories(botDB *db) {
+
     _db_handler = db;
-    _categories = _getCategoriesFromDB ();
+    _categories = _getCategoriesFromDB();
 
 }
 
