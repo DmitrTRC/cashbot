655ff57e6c7e9661f0687c1dc27f96e08440efe2
Dmitry Morozov <9984398@gmail.com>
2022-10-08 23:49:59 +0300

    Test Insert : Failed

--- tests/db_test.cpp
+++ tests/db_test.cpp
@@ -7,13 +7,54 @@
 
 TEST(DBTest, TestDB) {
 
+    auto *db = new botDB();
 
-    botDB db;
+    EXPECT_EQ(db->getCallState(), true);
 
-    EXPECT_EQ(db.getCallState(), true);
+    db->setCallState(false);
 
-    db.setCallState(false);
+    EXPECT_EQ(db->getCallState(), false);
 
-    EXPECT_EQ(db.getCallState(), false);
+}
+/**
+ sql = "create table expense("
+          "    id integer primary key, "
+          "    user_id integer default 641480282, "
+          "    amount integer, "
+          "    created datetime, "
+          "    category_codename integer, "
+          "    raw_text text, "
+          "    FOREIGN KEY(category_codename) REFERENCES category(codename) "
+          ");";
+          */
+TEST(DBTestInsert, TestDBInsertExpense) {
+
+    auto *db = new botDB();
+
+    std::map<std::string, std::string> expense = {
+            {"user_id",           "641480282"},
+            {"amount",            "100"},
+            {"created",           "2021-08-10"},
+            {"category_codename", "food"},
+            {"raw_text",          "test"}
+    };
+
+    db->insertRow("expense", expense);
+
+    std::vector<std::string> columns = {"id", "user_id", "amount", "created", "category_codename", "raw_text"};
+
+    auto result = db->fetchAll("expense", columns);
+
+    EXPECT_EQ(result.size(), 1);
+
+    EXPECT_EQ(result[0]["user_id"], "641480282");
+
+    EXPECT_EQ(result[0]["amount"], "100");
+
+    EXPECT_EQ(result[0]["created"], "2021-08-10 12:00:00");
+
+    EXPECT_EQ(result[0]["category_codename"], "food");
+
+    EXPECT_EQ(result[0]["raw_text"], "test");
 
 }
