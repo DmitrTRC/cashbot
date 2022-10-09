6776294cafdccb42ecf38b21fba7d5879935ee3f
Dmitry Morozov <9984398@gmail.com>
2022-10-09 21:25:10 +0300

    DB : Insert Expense Row : Fixed Test : PASSED

--- tests/db_test.cpp
+++ tests/db_test.cpp
@@ -34,37 +34,37 @@ TEST(DBTest, TestDB) {
           "    FOREIGN KEY(category_codename) REFERENCES category(codename) "
           ");";
           */
-//TEST(DBTestInsert, TestDBInsertExpenseRow) {
-//
-//    auto *db = new botDB();
-//
-//    std::map<std::string, std::string> expense = {
-//            {"user_id",           "641480282"},
-//            {"amount",            "100"},
-//            {"created",           "2021-08-10"},
-//            {"category_codename", "food"},
-//            {"raw_text",          "test"}
-//    };
-//
-//    db->insertRow("expense", expense);
-//
-//    std::vector<std::string> columns = {"id", "user_id", "amount", "created", "category_codename", "raw_text"};
-//
-//    auto result = db->fetchAll("expense", columns);
-//
-//    EXPECT_EQ(result.size(), 1);
-//
-//    EXPECT_EQ(result[0]["user_id"], "641480282");
-//
-//    EXPECT_EQ(result[0]["amount"], "100");
-//
-//    EXPECT_EQ(result[0]["created"], "2021-08-10 12:00:00");
-//
-//    EXPECT_EQ(result[0]["category_codename"], "food");
-//
-//    EXPECT_EQ(result[0]["raw_text"], "test");
-//
-//}
+TEST(DBTestInsert, TestDBInsertExpenseRow) {
+
+    auto *db = new botDB();
+
+    std::map<std::string, std::string> expense = {
+            {"user_id",           "641480282"},
+            {"amount",            "100"},
+            {"created",           "2021-08-10"},
+            {"category_codename", "food"},
+            {"raw_text",          "test"}
+    };
+
+    db->insertRow("expense", expense);
+
+    std::vector<std::string> columns = {"id", "user_id", "amount", "created", "category_codename", "raw_text"};
+
+    auto result = db->fetchAll("expense", columns);
+
+    EXPECT_EQ(result.size(), 1);
+
+    EXPECT_EQ(result[0]["user_id"], "641480282");
+
+    EXPECT_EQ(result[0]["amount"], "100");
+
+    EXPECT_EQ(result[0]["created"], "2021-08-10");
+
+    EXPECT_EQ(result[0]["category_codename"], "food");
+
+    EXPECT_EQ(result[0]["raw_text"], "test");
+
+}
 
 TEST(DBTestInsert, TestDBInsertBudgetRow) {
 
