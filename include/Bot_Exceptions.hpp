d678bdb188f190d4abd4e2cb6c9838d4168482d8
Dmitry Morozov <9984398@gmail.com>
2022-10-12 19:19:30 +0300

    Added Exceptions

---
+++ include/Bot_Exceptions.hpp
@@ -0,0 +1,24 @@
+//
+// Created by Dmitry Morozov on 12/10/22.
+//
+
+#ifndef CASHBOT_FEATURE_EXPENSE_BOT_EXCEPTIONS_HPP
+#define CASHBOT_FEATURE_EXPENSE_BOT_EXCEPTIONS_HPP
+
+#include <exception>
+
+class WrongMsgException : public std::exception {
+private:
+    char *message;
+
+public:
+    WrongMsgException(char *msg) : message(msg) {}
+
+    const char *what() const noexcept override {
+
+        return message;
+    }
+};
+
+
+#endif //CASHBOT_FEATURE_EXPENSE_BOT_EXCEPTIONS_HPP
---
+++ src/Bot_Exceptions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dmitry Morozov on 12/10/22.
+//
+
+#include "Bot_Exceptions.hpp"
