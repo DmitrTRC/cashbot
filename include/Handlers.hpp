7d5c36f90c744afed014bd8f4ccac9f079645a4b
Dmitry Morozov <9984398@gmail.com>
2022-10-31 13:02:27 +0300

    Handlers separating : Started

---
+++ include/Handlers.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Dmitry Morozov on 31/10/22.
+//
+
+#ifndef CASHBOT_FEATURE_EXPENSE_HANDLERS_HPP
+#define CASHBOT_FEATURE_EXPENSE_HANDLERS_HPP
+
+#include <tgbot/tgbot.h>
+
+void handleHelpCommand(TgBot::Bot *_bot, const TgBot::Message::Ptr &message);
+
+
+#endif //CASHBOT_FEATURE_EXPENSE_HANDLERS_HPP
---
+++ src/Handlers.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dmitry Morozov on 31/10/22.
+//
+
+#include "Handlers.hpp"
