d0b510c03355316ab8ee9a1b5200544cc70ca432
Dmitry Morozov <9984398@gmail.com>
2022-11-02 09:30:08 +0300

    Categories.cpp : Refactored Doc : added

--- include/Categories.hpp
+++ include/Categories.hpp
@@ -8,33 +8,58 @@
 #include "db.hpp"
 #include "db_structure.hpp"
 
-#include <vector>
 #include <map>
 #include <string>
+#include <vector>
 
-
+/**
+ * @brief Class for working with the categories table
+ */
 class Categories {
 public:
+    /**
+     * @brief Constructor
+     * @param db Pointer to the database
+     */
     explicit Categories(botDB *db);
 
+
+    ///<Destructor
     ~Categories() = default;
 
+    /**
+     * @brief Get all categories from the database
+     * @returns Vector of categories
+     */
     DB::TCategories getAllCategories();
 
+    /**
+     * @brief Get a category by its name
+     * @returns Category
+     */
     DB::Category getCategory(const std::string &);
 
 
 private:
-    botDB *_db_handler;
+    ///< Pointer to the database
+    botDB *_db_handler; ///< Pointer to the database
 
-    DB::TCategories _categories;
+    ///< Vector of categories
+    DB::TCategories _categories; ///< Vector of categories
 
+    /**
+     * @brief Fill alias names for categories
+     * @returns Vector of categories
+     */
     static DB::TCategories _fill_aliases(std::vector<std::map<std::string, std::string>> &categories);
 
+
+    /**
+     * @brief Load all categories from the database
+     * @returns Vector of categories
+     */
     DB::TCategories _getCategoriesFromDB();
 
-
 };
-
 
 #endif //CASHBOT_FEATURE_DB_CATEGORIES_HPP
--- src/Categories.cpp
+++ src/Categories.cpp
@@ -7,12 +7,20 @@
 
 #include <boost/algorithm/string.hpp>
 
-
+/**
+ * @brief Get all categories from the database
+ * @returns Vector of Categories
+ */
 DB::TCategories Categories::getAllCategories() {
 
     return _categories;
 }
 
+/**
+ * @brief Get a category by its name
+ * @param categoryName Category name string
+ * @returns Category
+ */
 DB::Category Categories::getCategory(const std::string &categoryName) {
 
     DB::Category resultCategory;
@@ -40,6 +48,10 @@ DB::Category Categories::getCategory(const std::string &categoryName) {
 }
 
 
+/**
+ * @brief fill alias names for categories
+ * @param categories Vector of categories maps
+ */
 DB::TCategories Categories::_fill_aliases(std::vector<std::map<std::string, std::string>> &categories) {
 
     DB::TCategories resultCategories;
@@ -58,9 +70,14 @@ DB::TCategories Categories::_fill_aliases(std::vector<std::map<std::string, std:
     }
 
     return resultCategories;
-
 }
 
+
+/**
+ * It fetches all categories from the database and returns them as a `TCategories` object
+ *
+ * @return It's a vector of maps.
+ */
 DB::TCategories Categories::_getCategoriesFromDB() {
 
     std::vector<std::string> columns = {"codename", "name", "is_base_expense", "aliases"};
@@ -72,6 +89,12 @@ DB::TCategories Categories::_getCategoriesFromDB() {
     return {result}; //TODO: return result! (now it's empty)
 }
 
+/**
+ * It creates a new Categories object, which is a list of all the categories in the database
+ *
+ * @brief Constructor
+ * @param db a pointer to the botDB object that was created in the main function.
+ */
 Categories::Categories(botDB *db) {
 
     _db_handler = db;
