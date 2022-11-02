5b3dc7009f141176f5e56dd42ae2a3bccba3a211
Dmitry Morozov <9984398@gmail.com>
2022-11-02 16:40:06 +0300

    Expense.hpp : Done db_structure : Done
    Refactored
    doc : Added

--- include/db_structure.hpp
+++ include/db_structure.hpp
@@ -5,41 +5,48 @@
 #ifndef CASHBOT_FEATURE_EXPENSE_DB_STRUCTURE_HPP
 #define CASHBOT_FEATURE_EXPENSE_DB_STRUCTURE_HPP
 
-#include <vector>
 #include <string>
+#include <vector>
 
+namespace DB { ///< The namespace for the database
 
-namespace DB {
-
+    /**
+     * The structure for the expense table
+     */
     struct DBExpense {
-        long id;
-        long user_id;
-        long amount;
-        std::string created;
-        std::string category_codename;
-        std::string raw_text;
+        long id; ///< The ID of the expense
+        long user_id; ///< The ID of the user who created the expense
+        long amount; ///< The amount of the expense
+        std::string created; ///< The date when the expense was created
+        std::string category_codename; ///< The codename of the category
+        std::string raw_text; ///< The raw text of the expense
     };
 
 
-    using TExpenses = std::vector<DBExpense>;
+    using TExpenses = std::vector<DBExpense>; ///< The type for the vector of expenses
 
+    /**
+     * The structure for the category table
+     */
     struct Category {
-        std::string codename;
-        std::string name;
-        bool is_base_expense;
-        std::vector<std::string> aliases;
+        std::string codename; ///< The codename of the category
+        std::string name; ///< The name of the category
+        bool is_base_expense; ///< Whether the category is a base expense
+        std::vector<std::string> aliases; ///< The aliases of the category
     };
 
-    using TCategories = std::vector<Category>;
+    using TCategories = std::vector<Category>; ///< The type for the categories vector
 
+    /**
+     * The structure for the Budget table
+     */
     struct Budget {
-        std::string codename;
-        long daily_limit;
+        std::string codename; ///< The codename of the budget
+        long daily_limit; ///< The daily limit of the budget
     };
 
-    using TBudgets = std::vector<Budget>;
+    using TBudgets = std::vector<Budget>; ///< The type for the budgets vector
 
 }
-
 
 #endif //CASHBOT_FEATURE_EXPENSE_DB_STRUCTURE_HPP
--- src/Expense.cpp
+++ src/Expense.cpp
@@ -75,10 +75,17 @@ std::string Expense::getDateToStr(time_t now_time) {
     return {date};
 }
 
-///Parse message to get amount and category
+
+/**
+ * It takes a string, checks if it matches the regex, and if it does, it extracts the amount and the category text from the
+ * string and returns them in a Message struct
+ *
+ * @param message The message to be parsed.
+ * @returns Message The parsed message.
+ */
 Message Expense::ParseMsg(const std::string &message) {
 
-    std::regex r(R"(([\d ]+) (.*))");
+    std::regex r(R"(([\d ]+) (.*))"); // The regex to match the message
 
     std::smatch match;
     std::regex_search(message, match, r);
@@ -96,9 +103,15 @@ Message Expense::ParseMsg(const std::string &message) {
 }
 
 
-// Returns string with today expenses
+/**
+ * It returns a string with today's expenses
+ *
+ * @return string with today expenses
+ */
 std::string Expense::get_today_stat() { //TODO: implement
+
     std::string SQL = "select sum(amount) from expense where date(created) = date('now', 'localtime');";
+
     auto result = _db_handler->fetchOne(SQL);
 
     if (result.empty()) {
@@ -115,6 +128,7 @@ std::string Expense::get_today_stat() { //TODO: implement
     result = _db_handler->fetchOne(SQL);
 
     std::string base_expenses;
+
     if (result.empty()) {
         base_expenses = "0";
     } else {
@@ -128,6 +142,13 @@ std::string Expense::get_today_stat() { //TODO: implement
     return result_str;
 }
 
+/**
+ * It takes a DBExpense struct and returns a map of strings to strings
+ *
+ * @param expense The expense object to be converted to SQL data.
+ *
+ * @return A map of strings to strings.
+ */
 std::map<std::string, std::string> Expense::getSQLExpense(const DB::DBExpense &expense) {
 
     std::map<std::string, std::string> result;
@@ -139,9 +160,13 @@ std::map<std::string, std::string> Expense::getSQLExpense(const DB::DBExpense &e
     result["raw_text"] = expense.raw_text;
 
     return result;
-
 }
 
+/**
+ * It fetches the daily limit from the database and returns it
+ *
+ * @return The daily budget limit.
+ */
 long Expense::getBudgetLimit() {
 
     std::vector<std::string> columns = {"daily_limit"};
@@ -151,6 +176,11 @@ long Expense::getBudgetLimit() {
     return std::stol(result);
 }
 
+/**
+ * It returns the last 5 expenses from the database
+ *
+ * @return A vector of TExpenses
+ */
 DB::TExpenses Expense::getLast() {
 
     auto result = getAllExpenses(5);
@@ -158,8 +188,16 @@ DB::TExpenses Expense::getLast() {
     return result;
 }
 
+/**
+ * It fetches all expenses from the database, and returns them as a vector of DB::DBExpense objects
+ * if the limit is not specified, it returns 100 expenses
+ * if needed, it can be changed to return all expenses to file
+ *
+ * @param limit The number of rows to return.
+ *
+ * @return DB::TExpenses
+ */
 DB::TExpenses Expense::getAllExpenses(const int &limit) {
-
 
     std::string SQL = "select e.id, e.amount, c.name, e.created "
                       "from expense e left join category c "
@@ -167,7 +205,9 @@ DB::TExpenses Expense::getAllExpenses(const int &limit) {
                       "order by created desc limit " + std::to_string(limit);
 
     auto result = _db_handler->fetchMany(SQL);
+
     DB::TExpenses expenses;
+
     for (auto row: result) {
         DB::DBExpense expense = {
                 .id = std::stol(row["id"]),
@@ -178,11 +218,11 @@ DB::TExpenses Expense::getAllExpenses(const int &limit) {
                 .raw_text = row["raw_text"]
 
         };
+
         expenses.push_back(expense);
     }
 
     return expenses;
-
 }
 
 void Expense::deleteExpense(const long &id) {
@@ -191,9 +231,16 @@ void Expense::deleteExpense(const long &id) {
 
 }
 
+/**
+ * It returns a string with the total amount of expenses for the current month, the total amount of base expenses for the
+ * current month and the amount of money left from the budget for the current month
+ *
+ * @return std::string
+ */
 std::string Expense::get_month_stat() {
 
     std::string SQL = "select sum(amount) from expense where strftime('%m', created) = strftime('%m', 'now', 'localtime');";
+
     auto result = _db_handler->fetchOne(SQL);
 
     if (result.empty()) {
@@ -210,6 +257,7 @@ std::string Expense::get_month_stat() {
     result = _db_handler->fetchOne(SQL);
 
     std::string base_expenses;
+
     if (result.empty()) {
         base_expenses = "0";
     } else {
@@ -222,6 +270,11 @@ std::string Expense::get_month_stat() {
     return result_str;
 }
 
+/**
+ * This function returns a pointer to the Categories object that is associated with the Expense object
+ *
+ * @return A pointer to the _categories object.
+ */
 Categories *Expense::getCategoriesPtr() {
 
     return _categories;
