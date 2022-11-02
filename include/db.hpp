7fa3d06da05087a304a9ba276fba71dc40e2d886
Dmitry Morozov <9984398@gmail.com>
2022-11-02 09:56:11 +0300

    db.hpp : Refactored doc : Added

--- include/db.hpp
+++ include/db.hpp
@@ -11,16 +11,16 @@
 #include <boost/algorithm/string/join.hpp>
 #include <iostream>
 #include <map>
-#include <string>
 #include <sqlite3.h>
+#include <string>
 
 
-/// Path to the database file
+///< Constexpr for the database name
 constexpr auto DB_PATH = "../db/cashbot.db";
 
-//TODO: Need to be Atomic
+
 /**
- * @brief Class for working with the database
+ * @brief Class ORM for working with the database
  */
 class botDB {
 
@@ -31,58 +31,119 @@ public:
      */
     botDB() : botDB(DB_PATH) {};
 
+    ///< Constructor with path to the database file
     explicit botDB(const std::string &db_path);
 
+    ///< Destructor
     ~botDB();
 
-    ///Insert Row in any table
+    /**
+     * @brief Insert a new record into the database
+     * @param table Table name
+     * @param values Map of values
+     */
     void insertRow(const std::string &table, const std::map<std::string, std::string> &);
 
+/**
+ * @brief delete a record from the database
+ * @param table Table name
+ * @param id ID of the record
+ */
     void deleteRow(const std::string &, const long &id);
 
-    //Get one raw from SQL request
+    /**
+     * @brief Fetch one record from the database
+     * @param SQL_query SQL query
+     * @returns Map of values
+     */
     std::map<std::string, std::string> fetchOne(std::string);
 
+    /**
+     * @brief Fills base categories table with default values
+     */
     void fill_base_categories();
 
+
+    ///< Clearing the database.
     void clearDB();
 
 
-    ///Fetch all rows from any table
+    /**
+     * @brief Fetch all records from the database
+     * @param table Table name
+     * @param columns Vector of columns
+     * @returns Vector of maps of values
+     */
     std::vector<std::map<std::string, std::string >>
     fetchAll(const std::string &table, const std::vector<std::string> &);
 
+    /**
+     * @brief Fetch many records from the database
+     * @param SQL_request SQL request
+     * @returns Vector of maps of values
+     */
     std::vector<std::map<std::string, std::string>> fetchMany(const std::string &SQL_request);
 
+    /**
+     * @brief Getter for CallState
+     * @return bool CallState value
+     */
     [[nodiscard]] bool getCallState() const;
 
+    /**
+     * @brief Setter for CallState
+     * @param CallState value
+     */
     void setCallState(bool callState);
 
+    /**
+     * @brief Returns current DB Path
+     * @return std::string DB Path
+     */
     [[nodiscard]] std::string getDBPath() const;
 
+    /**
+     * @brief Fill Example DB
+     */
     void fill_test_tables();
 
+    /**
+     * @brief delete all records from the database
+     */
     void deleteDB();
 
 
 private:
 
-    sqlite3 *_bot_db;
+    sqlite3 *_bot_db; ///< Pointer to the database
 
-    std::string _bot_db_path;
+    std::string _bot_db_path; ///< Path to the database file
 
+    /**
+     * @brief Open the database
+     * @return Pointer to the database
+     */
     sqlite3 *_openDB();
 
+    /**
+     * @brief Close the database
+     */
     void _closeDB();
 
+    /**
+     * @brief Init the database
+     */
     void _initDB();
 
+    /**
+     * @brief Check if the database is exist
+     * @return True if the database is exist
+     */
     bool _check_db_exists();
 
-    bool _call_state = false;
 
+    bool _call_state = false; ///< Call state for the database Exec callback
 
 };
-
 
 #endif //CASHBOT_FEATURE_DB_DB_HPP
