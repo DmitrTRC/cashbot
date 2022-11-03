5d7eea695731faa94b6b775a1a4dba516e6b6e9d
Dmitry Morozov <9984398@gmail.com>
2022-11-03 20:21:10 +0300

    Want Fix for TODAY

--- src/Helper.cpp
+++ src/Helper.cpp
@@ -4,6 +4,8 @@
 
 #include "Helper.hpp"
 
+#include <tgbot/tgbot.h>
+
 #include <iomanip>
 #include <sstream>
 
@@ -14,8 +16,12 @@
  * @returns std::string that contains the help message.
  */
 std::string Helper::helpMessage() {
+
+
+
+
     ///Greetings message
-    std::string message = "Hello. I'm a bot that helps you to track your expenses. \n"
+    std::string message = "Hello. I'm a bot that helps you to track your expenses.  \n"
                           "Here is the list of commands that I can understand:\n\n"
                           "<i>For adding an expense:</i>\n"
                           "<b>Example</b> : 2000 food\n\n"
@@ -31,3 +37,10 @@ std::string Helper::helpMessage() {
 
     return message + command_stream.str();
 }
+
+//TgBot::Sticker::Ptr botSticker(const std::string &sticker_id) {
+//
+//    TgBot::Sticker::Ptr sticker(new TgBot::Sticker);
+//    sticker->emoji = "🤖";
+//    return sticker;
+//}
